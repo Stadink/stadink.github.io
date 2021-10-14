99489ec564f1c06c62bcd780bee3f050b2999c4c
Egor <egor.stadnik@tommystachi.cz>
2021-10-14 19:42:32 +0200

    testing stuff

--- src/App.js
+++ src/App.js
@@ -8,19 +8,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {console.log(<Contact />)}
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to FUCKING ROCK OMG you're actually doing it in real time damn.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+
+        actually this is very cool
+
         <Contact />
         <Clock />
       </header>
