43aa9e38d5d7113dc2d0af02ee814a571172a3b5
Egor <egor.stadnik@tommystachi.cz>
2021-10-17 00:00:46 +0200

    stupid buillshit, bad idea

--- src/Sandbox/Image.js
+++ src/Sandbox/Image.js
@@ -9,7 +9,8 @@ export class Image extends React.Component {
       this.state = {
           src: require(`../img/2.jpg`).default,
           num: 0,
-          company: 'idk'            
+          company: 'Xenonlord Corporation',
+          symbol: 'idk'            
       };
 
       this.setRandomNum = this.setRandomNum.bind(this) 
@@ -28,10 +29,14 @@ export class Image extends React.Component {
     setRandomImage() {
       const randomNum = Math.floor(Math.random()*949);
       const name = data[randomNum]['Company name']
+      const abbreviation = data[randomNum].Symbol
+      const api_key = '373a163fab81660785ca5880ef50be1e63025ffbef46c957f33c40a4619f4829'
+      const url = `https://serpapi.com/search.json?q=${abbreviation} logo&tbm=isch&ijn=0&api_key=${api_key}`
 
       this.setState({ 
         num: randomNum,
         company: name,
+        symbol: abbreviation,
         src: `https://logo.clearbit.com/${name}.com`,
       })
     }
@@ -39,7 +44,7 @@ export class Image extends React.Component {
   render() {
     return (
       <div id="image">
-          {this.state.num} {this.state.company} <br />
+          {this.state.num}. {this.state.company} [{this.state.symbol}] <br />
           <img onClick={this.setRandomNum} id="artImg" src={this.state.src} />
       
           <div id="answerButtons">
