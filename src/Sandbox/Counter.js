b5e0ece065ad28c6fad304da3f84c35e28b97990
Your Name <you@example.com>
2021-10-18 22:54:45 +0200

    added counter from Vika's Laptop

---
+++ src/Sandbox/Counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class Counter extends React.Component {
+    getTimeRemaining(endtime){
+        const total = Date.parse(endtime) - Date.parse(new Date());
+        const days = Math.floor( total/(1000*60*60*24) );
+        return days
+    }
+    render() {
+        return (
+            <p>Day #{ this.getTimeRemaining('May 18, 2045') }</p>
+        );
+  }
+}
