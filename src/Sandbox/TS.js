56b0f3705f911e5e3ba5c28b1f70089ae380e1b0
Your Name <you@example.com>
2021-11-07 10:28:53 +0100

    Added daily/weekly task working properly

--- src/Sandbox/TS.js
+++ src/Sandbox/TS.js
@@ -4,18 +4,33 @@ import TSdata from './TS.json';
 export class TS extends React.Component {
 
     printLesson() {
-        const day = Math.round(Math.random() * 7)
+        const d = new Date();
 
+        const weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+
+        let day = weekday[d.getDay()];
         const messageToAlert = TSdata[day]
 
         alert(messageToAlert)
     }
 
+    printTask() {
+        alert(TSdata['Task'])
+    }
+
   render() {
     return (
         <div id="TS">
-            <h3>Week #2 Belief or experience?</h3>
-            <button onClick={ () => this.printLesson() }>TS</button>
+            <h3>Week #3 Reactions</h3>
+            <button onClick={ () => this.printTask() }>Week Task</button>
+            <button onClick={ () => this.printLesson() }>Day Lesson</button>
         </div>
     );
   }
