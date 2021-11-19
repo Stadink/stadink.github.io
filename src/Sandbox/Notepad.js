da91caef690b5634b44bc452d8d39517ec344056
Stadnego <barbaris181@gmail.com>
2021-11-19 23:27:30 +0100

    Huge progress, i'm thrilled and excited and anxious and horny and all those emotions

--- src/Sandbox/Notepad.js
+++ src/Sandbox/Notepad.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import { collection, onSnapshot, setDoc, doc } from '@firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import db from './firebase';
+import moment from 'moment';
 
 
 export class Notepad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'Use me'};
+    this.handleNew = this.handleNew.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {    
+    this.setState({value: event.target.value});  
+  }
+  
+  async handleNew() {
+    // const notepadText = await document.querySelector('#notepad').innerHTML;
+
+    const docRef = doc(db, "SDSlog", moment().toString());
+    const payload = {idea: this.state.value}
+    await setDoc(docRef, payload);
+
+  }
+
   render() {
     return (
-        <div id="notepad" contenteditable="true" autocomplete="off">
-            I want this text to get saved to the database.
-            <br/><br/>
-            (what would I type here though? Practice with obsidian)
-        </div>
+      <div id="notepadSection">
+        <textarea id="notepad" contenteditable="true" autocomplete="off" onChange={this.handleChange}>
+          {this.state.value}
+        </textarea> <br/>
+        <button className="button" onClick={this.handleNew}>New</button>
+      </div>
+        
     );
   }
 }
