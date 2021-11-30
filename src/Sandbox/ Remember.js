9680309d4644512790010d29c034b5638d71f709
Stadnego <barbaris181@gmail.com>
2021-11-30 02:20:01 +0100

    PM based on checkboxes FE done

--- src/Sandbox/ Remember.js
+++ src/Sandbox/ Remember.js
@@ -8,16 +8,17 @@ export class Remember extends React.Component {
   render() {
     return (
         <div id="Remember">
-          <details>
+          <details open>
             <summary>Important</summary>
               💯% <br/>
               ⭐ Switch to thoughts in English
 
               <br/><br/>
               What can I do today to be satisfied with this day ending? <br/>
-              <input type="checkbox"  /> <a href='https://www.codecademy.com/learn' target="_blank">Codecademy</a> <br/>
-              <input type="checkbox"  /> SDS <br/>
-              <input type="checkbox"  /> 10 mins of random Obsidian <br/>
+              <input id='Codecademy' type="checkbox"  /> <a href='https://www.codecademy.com/learn' target="_blank">Codecademy</a> <br/>
+              <input id='SDS' type="checkbox"  /> SDS <br/>
+              <input id='Sport' type="checkbox"  /> Sport <br/>
+              <input id='Obsidian' type="checkbox"  /> 10 mins of random Obsidian <br/>
             </details> <br />
             {/* <TS /> */}
             <TranscendingSelf />
--- src/Sandbox/Counter.js
+++ src/Sandbox/Counter.js
@@ -7,9 +7,29 @@ export class Counter extends React.Component {
         const days = Math.floor( total/(1000*60*60*24) );
         return days
     }
+
+    checkboxesCrossed() {
+        let checkboxesCrossed = 0;
+
+        if(document.getElementById('Codecademy') !== null) {
+            checkboxesCrossed = document.getElementById('Codecademy').checked ? checkboxesCrossed+1 : checkboxesCrossed;
+            checkboxesCrossed = document.getElementById('SDS').checked ? checkboxesCrossed+1 : checkboxesCrossed;
+            checkboxesCrossed = document.getElementById('Sport').checked ? checkboxesCrossed+1 : checkboxesCrossed;
+            checkboxesCrossed = document.getElementById('Obsidian').checked ? checkboxesCrossed+1 : checkboxesCrossed;
+        }
+        return checkboxesCrossed
+    }
+
     render() {
         return (
-            <div>Day #<u>{ this.getTimeRemaining('May 18, 2045')}</u></div>
+            <div>Day #<u>{ this.getTimeRemaining('May 18, 2045')}</u> | PM: {this.checkboxesCrossed()} </div>
         );
   }
+    componentDidMount() {
+        // Paste your code here.
+        const oneSecond = 1000;
+        setInterval(() => {
+        this.setState({ date: new Date() });
+        }, oneSecond);
+    }
 }
