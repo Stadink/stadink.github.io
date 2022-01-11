29e0c4f5525af6ea701659c955491ad9ee4e32e7
Stadnego <barbaris181@gmail.com>
2022-01-11 17:49:35 +0100

    added goals

--- src/Sandbox/Goals.js
+++ src/Sandbox/Goals.js
@@ -15,10 +15,14 @@ export class Goals extends React.Component {
         <div style={{'border' : '1px solid white', 'margin-left' : '10px'}} id="Goals">
             <h1><u>Goals</u></h1>
 
-            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Ralston's CAP</h2><br />
+            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Ralston's CAP while it's possible...</h2><br />
             <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Own 1 share of Tesla</h2><br />
             <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Go to USA 🇺🇸</h2><br />
             <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Buy <a href="http://TheCosmicJoke.com" target="_blank">TheCosmicJoke.com</a></h2><br />
+            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Get a black belt</h2><br />
+            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Get a Ph.D in AI</h2><br />
+            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Travel the world</h2><br />
+
 
             <br />
             <br />
