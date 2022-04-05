062b349919c8119e84bf327c0156bae206c26ba1
Stadnego <barbaris181@gmail.com>
2022-04-05 23:54:45 +0200

    updated goals

--- src/Sandbox/Goals.js
+++ src/Sandbox/Goals.js
@@ -15,17 +15,19 @@ export class Goals extends React.Component {
         <div style={{'border' : '1px solid white', 'margin-left' : '10px'}} id="Goals">
             <h1><u>Goals</u></h1>
 
-            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >OWN some Bitcoin</h2><br />
-            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Learn Swift</h2><br />
+            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >💲⚙️💱  while 🧘‍♂️</h2><br />
+            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Go to USA 🇺🇸</h2><br />
+            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Get a black belt 🥋</h2><br />
+            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Get a Ph.D in AI</h2><br />
+
+            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Get Enlightened 👁️</h2><br />
+            {/* <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Learn Swift</h2><br />
             <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Learn React & NodeJS</h2><br />
             <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Ralston's CAP while it's possible...</h2><br />
             <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Own 1 share of Tesla</h2><br />
-            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Go to USA 🇺🇸</h2><br />
             <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Buy <a href="http://TheCosmicJoke.com" target="_blank">TheCosmicJoke.com</a></h2><br />
-            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Get a black belt 🥋</h2><br />
-            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Get a Ph.D in AI</h2><br />
             <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Travel the world ✈️</h2><br />
-            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Fill my ISI with Xenon</h2><br />
+            <input type="checkbox"></input> <h2 style={{'display': 'inline'}} >Fill my ISI with Xenon</h2><br /> */}
 
 
             <br />
