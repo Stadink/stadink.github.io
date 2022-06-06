ca5527ea26218798a6aa483d38a3b13f2a5a7c1c
egorstadnik <egor.stadnik@tommystachi.cz>
2022-06-06 21:02:07 +0200

    get back here mofo tab message

--- src/Sandbox/Clock.js
+++ src/Sandbox/Clock.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 export class Clock extends React.Component {
   constructor(props) {
@@ -13,6 +14,10 @@ export class Clock extends React.Component {
     const oneSecond = 1000;
     setInterval(() => {
       this.setState({ date: new Date() });
+      if(document.hidden) 
+        document.title = 'Get back here mofo'
+      else 
+        document.title = this.state.date
     }, oneSecond);
   }
 }
