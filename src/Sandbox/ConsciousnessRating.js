9560a54816f7e70637d5e0dd3e8478f983576b0d
egorstadnik <egor.stadnik@tommystachi.cz>
2022-07-21 05:09:21 +0200

    removed console button

--- src/Sandbox/ConsciousnessRating.js
+++ src/Sandbox/ConsciousnessRating.js
@@ -8,64 +8,85 @@ export class ConsciousnessRating extends React.Component {
     this.state = {
       value: 50,
       comment: '',
+      allNumbers: this.getAllNumbers(),
     };
     this.updateValue = this.updateValue.bind(this);
+    this.getAverage();
   }
 
   updateValue() {
     var slider = document.getElementById("myRange");
-    var output = document.getElementById("demo");
-
     this.setState({value: slider.value});
   }
 
-  getLastTenAverage() {
-    // !!!
+  async getAllNumbers() {
+    const docRef = doc(db, 'ConsciousnessRating', 'Log')
+    const docSnapshot = await getDoc(docRef)
+    const all = docSnapshot.data();
+
+    console.log('GetAllNumbers is: ' + JSON.stringify(all['History']))
+
+    this.setState({ allNumbers: all['History'] })
+
+    this.getAverage()
   }
 
-  setAverage() {
-    // get all ratings
-    // get last 10
-    // average
-    // extract last part
-    // return
+  getAverage() {
+    const all = this.state.allNumbers;
+    let sum = 0;
+
+    for(let i = 1; i <+ 10; i++) {
+      sum += parseInt(all[all.length - i])
+    }
+
+    const average = Math.round(sum / 10);
+    this.setState({value: average})
   }
 
   async saveToDb() {
     let comment = prompt(`Why ${this.state.value} tho?`, 'Well...');
-    this.setState({comment: comment});
+    if (comment != null) {
+      this.setState({comment: comment});
 
-    const day = document.querySelector('#DayNum').textContent;
-    const minute = document.querySelector('#minuteNumber').textContent;
-    const rating = this.state.value;
+      const day = document.querySelector('#DayNum').textContent;
+      const minute = document.querySelector('#minuteNumber').textContent;
+      const rating = this.state.value;
 
-    const consciousnessTimestamp = `${day}-${minute}-${rating}`
-    const docRef = doc(db, 'ConsciousnessRating', 'All')
+      const consciousnessTimestamp = `${day}-${minute}-${rating}`
+      const docRef = doc(db, 'ConsciousnessRating', 'All')
+      const docRefLog = doc(db, 'ConsciousnessRating', 'Log')
 
-    let obj = {};
-    obj[consciousnessTimestamp] = comment;
+      let payloadHistory = {History: arrayUnion(rating)};
 
-    let payload = obj;
-    await updateDoc(docRef, payload)
+      let obj = {};
+      obj[consciousnessTimestamp] = comment;
+
+      let payload = obj;
+      await updateDoc(docRef, payload)
+      await updateDoc(docRefLog, payloadHistory)
+    }
   }
+
+  handleEvent = async (event) => {
+    if (event.type === "mousedown") {
+           this.setState({ message: "Mouse Down"});
+       } else {
+           this.setState({ message: "Mouse Up"});
+           await this.saveToDb();
+       }
+   };
 
   render() {
     return (
         <div id="consciousnessRating">
           <div class="slidecontainer"> <br/>
-            <input onChange={this.updateValue} id="myRange" type="range" min="20" max="1000" value={this.state.value} class="slider" />
-
+            <input onMouseUp={ this.handleEvent } onChange={this.updateValue} id="myRange" type="range" min="20" max="1000" value={this.state.value} class="slider" />
               <details>
-                <summary><p>Consciousness rating: <span id="demo">{this.state.value} </span> <button class="button1" onClick={ () => { this.saveToDb() }}>+</button> </p></summary>
+                <summary><p>Consciousness rating: <span id="demo">{this.state.value} </span>  </p></summary>
                 <img src='https://willthisdofor.art/ConsciousnessRating.png'/>
               </details>
-
           </div>
         </div>
     );
-  }
-
-  componentDidMount() {
-
   }
 }
