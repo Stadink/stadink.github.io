fce796fc812c7a6ef571de9f112e1988e67a02c4
egorstadnik <egor.stadnik@tommystachi.cz>
2022-10-02 21:26:11 +0200

    color wiki button works, but the code is shit, and I won't waste time improving it now, hm.

--- src/Sandbox/Footer.js
+++ src/Sandbox/Footer.js
@@ -7,6 +7,7 @@ import { collection, onSnapshot, arrayUnion, updateDoc, doc, query} from '@fireb
 
 export const Footer = () => {
   const [colors, setColors] = useState([{ colors: ["Loading..."]}]);
+  const [currentColor, setCurrentColor] = useState('idk');
 
   const collectionRef = collection(db, "Colors");
   const q = query(collectionRef);
@@ -23,34 +24,81 @@ export const Footer = () => {
     const color = colors['0'].colors[random];
     console.log(color);
     document.body.style.backgroundColor = color;
+    setCurrentColor(color);
   }
 
 
-    const randomColor = () => {
-        var color = '#';
-        var letters = '0123456789ABCDEF';
-        
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        console.log(color);
-        document.body.style.backgroundColor = color;
-        // document.getElementById("colorName").innerText = color;
-    }
+  const randomColor = () => {
+      var color = '#';
+      var letters = '0123456789ABCDEF';
+      
+      for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+      }
+      console.log(color);
+      document.body.style.backgroundColor = color;
+      setCurrentColor(color)
+      // document.getElementById("colorName").innerText = color;
+  }
 
  
 
 
-    useEffect(
-        () => {
-            randomColor();
-        }
-    )
+  useEffect(
+      () => {
+          randomColor();
+      }, []
+  )
     
+  function valueToHex(c) {
+    var hex = c.toString(16);
+    return hex
+  }
+
+  function rgbToHex(rgbString) {
+    if (rgbString.includes('#')){
+      return rgbString
+    }
+
+    let noRgb = rgbString.replace('rgb', '')
+    let noBrackets = noRgb.replace('(', '')
+    noBrackets = noBrackets.replace(')', '')
+
+    let values = noBrackets.split(',')
+
+    let r = parseInt(values[0])
+    let g = parseInt(values[1])
+    let b = parseInt(values[2])
+
+    return(valueToHex(r).toUpperCase() + valueToHex(g).toUpperCase() + valueToHex(b).toUpperCase());
+  }
+
+  const saveColor = async () => {
+    // What's the difference between getDoc() and just doc() by the way?
+    const docRef = doc(db, 'Colors', 'Tarot');
+    const color = document.body.style.backgroundColor ;
+    let payload = {colors: arrayUnion(color)};
+    
+    await updateDoc(docRef, payload);
+  }
+
+  const setColor = (color) => {
+    document.body.style.backgroundColor = color;
+    setCurrentColor(color)
+  }
 
   return (
     <div id="footer">
-      <ColorPalette />
+      {/* <ColorPalette /> */}
+      <div id='colorPalette'>
+        {   
+            colors['0'].colors.map((color, index) => (
+              <button class="clickable" onClick={() => setColor(color)} style={{backgroundColor: color}} key={index}>⠀</button>
+            ))
+        }
+
+        <button onClick={() => saveColor()}>💾</button>
+    </div>
       <br />
       <Link to="/build">Dashboard</Link>   | 
       <Link to="/tarot">Tarot</Link>   | 
@@ -61,7 +109,8 @@ export const Footer = () => {
       <Link to="/art">Art</Link> |
       <br />
       <br />
-      <button onClick={() => randomSavedColor()}>Random color</button>
+      <button onClick={() => randomSavedColor()}>Random color</button> |
+      <a href={'https://www.colorhexa.com/' + rgbToHex(document.body.style.backgroundColor)} target="_blank">{rgbToHex(currentColor)}</a>
     </div>
   );
 };
